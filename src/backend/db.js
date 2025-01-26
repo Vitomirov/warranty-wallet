81460537922dc1f676397126411ace4712ea2824
Vitomirov <dejan.vitomirov@gmail.com>
2025-01-26 12:55:06 +0100

    .env file improvement- database info

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,11 +1,29 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for use in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: `${__dirname}/.env` });
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'devito', 
-  password: 'vitomirov1989',
-  database: 'warranty_db', 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
+
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+
 
 connection.connect((err) => {
   if (err) throw err;
