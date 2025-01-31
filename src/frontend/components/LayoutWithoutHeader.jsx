d4229409d81e598f121e4ef76aace45a5601b981
Vitomirov <dejan.vitomirov@gmail.com>
2025-01-31 16:26:35 +0100

    Save last version before Dockerization

--- src/frontend/components/LayoutWithoutHeader.jsx
+++ src/frontend/components/LayoutWithoutHeader.jsx
@@ -4,7 +4,6 @@ import Footer from './Footer';
 const LayoutWithoutHeader = ({ children }) => {
   return (
     <div className="d-flex flex-column min-vh-100">
-      {/* Main content fills available space */}
       <main className="d-flex flex-grow-1">
         {children}
           </main>
