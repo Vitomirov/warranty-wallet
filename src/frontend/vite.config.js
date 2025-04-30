fbd485e27231b604480633ae6fbe438b19000cd6
Vitomirov <dejan.vitomirov@gmail.com>
2025-04-30 12:11:15 +0200

    Dockerfile backend, vite.conf.js adjustment

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/',
-    build: {
-        outDir: 'dist',
-    },
-    server: {
-        https: {
-          key: './certs/localhost-key.pem', // Path to your private key
-          cert: './certs/localhost.pem',     // Path to your certificate
-        },
-        host: 'localhost', // Ensure Vite listens on localhost
-      },
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
 });
