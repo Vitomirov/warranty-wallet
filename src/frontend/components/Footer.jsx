7257cd42089cbbf02612d1afc12f10a49f805088
Vitomirov <dejan.vitomirov@gmail.com>
2025-05-15 10:27:23 +0000

    Footer adjustment for small screens

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -12,32 +12,33 @@ function Footer() {
   };
 
   return (
-    <footer
-      id="contact"
-      className="bg-gradient border-bottom border-dark py-2 d-flex justify-content-between align-items-center d-none d-md-flex"
-    >
-      <p className="mb-0 col-lg-5">
-        &copy; Designed and Developed by Dejan Vitomirov
-      </p>
-      <div className="pe-5">
-        <span
-          onClick={handleLinkedinClick}
-          style={{ cursor: "pointer", margin: "0 10px" }}
-        >
-          <i className="bi bi-linkedin" style={{ fontSize: "1.5rem" }}></i>
-        </span>
-        <span
-          onClick={handleGitHubClick}
-          style={{ cursor: "pointer", margin: "0 10px" }}
-        >
-          <i className="bi bi-github" style={{ fontSize: "1.5rem" }}></i>
-        </span>
-        <span
-          onClick={handleMailClick}
-          style={{ cursor: "pointer", margin: "0 10px" }}
-        >
-          <i className="bi bi-envelope" style={{ fontSize: "1.5rem" }}></i>
-        </span>
+        <footer className="bg-gradient border-top border-dark py-3">
+      <div className="container-fluid">
+        <div className="d-flex flex-column flex-md-row justify-content-between align-items-center text-center text-md-start">
+          <p className="mb-2 mb-md-0">
+            &copy; Designed and Developed by Dejan Vitomirov
+          </p>
+          <div className="me-lg-5">
+            <span
+              onClick={handleLinkedinClick}
+              style={{ cursor: "pointer", margin: "0 10px" }}
+            >
+              <i className="bi bi-linkedin" style={{ fontSize: "1.5rem" }}></i>
+            </span>
+            <span
+              onClick={handleGitHubClick}
+              style={{ cursor: "pointer", margin: "0 10px" }}
+            >
+              <i className="bi bi-github" style={{ fontSize: "1.5rem" }}></i>
+            </span>
+            <span
+              onClick={handleMailClick}
+              style={{ cursor: "pointer", margin: "0 10px" }}
+            >
+              <i className="bi bi-envelope" style={{ fontSize: "1.5rem" }}></i>
+            </span>
+          </div>
+        </div>
       </div>
     </footer>
   );
