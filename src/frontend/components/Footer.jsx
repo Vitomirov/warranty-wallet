f49cb90326c633a155d4580889b726f0dda9286e
Vitomirov <dejan.vitomirov@gmail.com>
2025-08-10 15:24:52 +0200

    Update footer and deploy.yml

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -1,47 +1,23 @@
 import React from "react";
 
-function Footer() {
-  const handleMailClick = () => {
-    window.open("mailto:dejan.vitomirov@gmail.com", "_target");
-  };
-  const handleGitHubClick = () => {
-    window.open("https://github.com/Vitomirov", "_target");
-  };
-  const handleLinkedinClick = () => {
-    window.open("https://www.linkedin.com/in/dejan-vitomirov/", "_target");
-  };
-
+const Footer = () => {
   return (
-    <footer className="bg-gradient border-top border-dark py-3">
-      <div className="container-fluid">
-        <div className="d-flex flex-column flex-md-row justify-content-between align-items-center text-center text-md-start">
-          <p className="footer-copy mb-2 mb-md-0">
-            &copy; Designed and Developed by Dejan Vitomirov
-          </p>
-          <div className="me-lg-5">
-            <span
-              onClick={handleLinkedinClick}
-              style={{ cursor: "pointer", margin: "0 10px" }}
-            >
-              <i className="bi bi-linkedin social-icon"></i>
-            </span>
-            <span
-              onClick={handleGitHubClick}
-              style={{ cursor: "pointer", margin: "0 10px" }}
-            >
-              <i className="bi bi-github social-icon"></i>
-            </span>
-            <span
-              onClick={handleMailClick}
-              style={{ cursor: "pointer", margin: "0 10px" }}
-            >
-              <i className="bi bi-envelope social-icon"></i>
-            </span>
-          </div>
-        </div>
+    <footer className="bg-gradient py-3">
+      <div className="d-flex justify-content-center align-items-center w-100">
+        <p className="footer-copy mb-2 mb-md-0 text-center">
+          &copy; Designed and Developed by{" "}
+          <a
+            className="text-decoration-underline text-white"
+            href="https://portfolio.devitowarranty.xyz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dejan Vitomirov
+          </a>
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
